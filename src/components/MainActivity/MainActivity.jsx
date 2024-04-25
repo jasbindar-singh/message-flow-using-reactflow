1e070aa0ff0655d45489f66fde7ff0ee8146bb13
Jasbindar Singh <jasbindar1998@gmail.com>
2024-04-26 02:51:51 +0530

    feat: enable adding nodes

--- src/components/MainActivity/MainActivity.jsx
+++ src/components/MainActivity/MainActivity.jsx
@@ -1,28 +1,57 @@
+import { useCallback, useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
-import ReactFlow, { useEdgesState, useNodesState } from "reactflow";
+import ReactFlow, {
+  ReactFlowProvider,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
+import { v4 } from "uuid";
 
 const MainActivity = () => {
-  const [, drop] = useDrop(() => ({
-    accept: "node-element",
-    drop: () => console.log("dropped"),
-  }));
-  const [, drag] = useDrag(() => ({
-    type: "node-element",
-    item: { id: "drag" },
-  }));
-
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState();
+
+  const onMessageDropped = useCallback(
+    (event) => {
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+
+      const newNode = {
+        id: v4(),
+        type: "default",
+        position,
+        data: { type: "text", value: "" },
+      };
+
+      setNodes((nodes) => nodes.concat(newNode));
+    },
+    [reactFlowInstance, setNodes]
+  );
+
+  const [, drop] = useDrop(() => ({
+    accept: "node",
+  }));
+  const [, drag] = useDrag(() => ({
+    type: "node",
+    item: { id: "drag" },
+  }));
 
   return (
     <div className="flex w-full flex-1">
       <div ref={drop} className="w-full">
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-        />
+        <ReactFlowProvider>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onInit={setReactFlowInstance}
+            onDrop={onMessageDropped}
+          />
+        </ReactFlowProvider>
       </div>
       <div className="w-[500px] border-2 border-gray-200">
         <button ref={drag}>Message</button>
