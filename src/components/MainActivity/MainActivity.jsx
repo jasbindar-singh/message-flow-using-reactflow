f77194685fdd421b9fc5f1387209fee8f86d1903
Jasbindar Singh <jasbindar1998@gmail.com>
2024-04-26 14:07:21 +0530

    feat: add text editor

--- src/components/MainActivity/MainActivity.jsx
+++ src/components/MainActivity/MainActivity.jsx
@@ -1,15 +1,13 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
-import ReactFlow, {
-  ReactFlowProvider,
-  useEdgesState,
-  useNodesState,
-} from "reactflow";
+import ReactFlow, { useEdgesState, useNodesState } from "reactflow";
 import { v4 } from "uuid";
 import CustomNode from "../CustomNode";
 
 const nodeTypes = { textMessage: CustomNode };
 const MainActivity = () => {
+  const [nodeText, setNodeText] = useState("");
+  const [selectedNodeId, setSelectedNodeId] = useState("");
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const [reactFlowInstance, setReactFlowInstance] = useState();
@@ -41,28 +39,75 @@ const MainActivity = () => {
     item: { id: "drag" },
   }));
 
+  const onNodeClick = (_, node) => {
+    setSelectedNodeId(node.id);
+    setNodeText(node.data.text);
+  };
+
+  const resetSelectedNode = () => {
+    setSelectedNodeId("");
+    setNodeText("");
+  };
+
+  useEffect(() => {
+    if (selectedNodeId)
+      setNodes((nds) =>
+        nds.map((node) => {
+          if (node.id === selectedNodeId) {
+            node.data = {
+              ...node.data,
+              text: nodeText,
+            };
+          }
+          return node;
+        })
+      );
+  }, [nodeText, selectedNodeId, setNodes]);
+
   return (
     <div className="flex w-full flex-1">
       <div ref={drop} className="w-full">
-        <ReactFlowProvider>
-          <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            nodeTypes={nodeTypes}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onInit={setReactFlowInstance}
-            onDrop={onMessageDropped}
-          />
-        </ReactFlowProvider>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          nodeTypes={nodeTypes}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onInit={setReactFlowInstance}
+          onDrop={onMessageDropped}
+          onNodeClick={onNodeClick}
+          onPaneClick={resetSelectedNode}
+        />
       </div>
       <div className="w-[500px] p-[16px] border-2 border-gray-200 flex justify-center items-start">
-        <button
-          ref={drag}
-          className="border-[1px] p-[24px] border-blue-400 text-blue-400 rounded-sm"
-        >
-          Message
-        </button>
+        {selectedNodeId ? (
+          <div className="w-full">
+            <div className="mb-[24px]">
+              <button
+                className="border-[1px] px-[12px] border-black rounded-sm bg-white"
+                onClick={resetSelectedNode}
+              >
+                Back
+              </button>
+            </div>
+            <div>
+              <textarea
+                value={nodeText}
+                onChange={(e) => setNodeText(e.target.value)}
+                rows={5}
+                className="w-full border-[1px] border-gray-400 p-[4px] rounded-sm"
+                placeholder="Message"
+              />
+            </div>
+          </div>
+        ) : (
+          <button
+            ref={drag}
+            className="border-[1px] p-[24px] border-blue-400 text-blue-400 rounded-sm"
+          >
+            Message
+          </button>
+        )}
       </div>
     </div>
   );
